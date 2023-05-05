632dbe18870fbd5ce1f28f78d82b7ca5d6a03518
V1NW0G <vincentwongck110@gmail.com>
2023-05-05 22:31:59 +0800

    updated

--- screens/Listing.js
+++ screens/Listing.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, Linking, ScrollView} from 'react-native';
 import axios from 'axios';
 import { useNavigation } from "@react-navigation/native";
+// import { setIntervalAsync, clearIntervalAsync } from 'set-interval-async';
+import moment from "moment";
+
 
 const Listing = () => {
 
@@ -11,16 +14,54 @@ const Listing = () => {
   const fetchData = () => {
     axios.get('https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/BFA3460955AC820C')
     .then(response => {
-        setData(response.data.data);
+        // Create an object where each key represents a unique route and the value is an array of all the ETAs for that route
+        const routeMap = {};
+        response.data.data.forEach(item => {
+            if (!routeMap[item.route]) {
+                routeMap[item.route] = [];
+            }
+            routeMap[item.route].push(item.eta);
+        });
+
+        // Sort each array of ETAs in ascending order and take the first element as the earliest ETA
+        const earliestETAs = [];
+        for (const route in routeMap) {
+            routeMap[route].sort();
+
+            const eta = routeMap[route][0];
+            const current = moment();
+            const diff = moment.duration(moment(eta).diff(current));
+            const minutes = diff.asMinutes();
+            console.log(minutes)
+
+            earliestETAs.push({
+                route: route,
+                dest_tc: response.data.data.find(item => item.route === route).dest_tc,
+                serviceType: response.data.data.find(item => item.route === route).service_type,
+                eta: routeMap[route][0],
+                minutesLeft: minutes
+            });
+        }
+
+        setData(earliestETAs);
     })
     .catch(error => {
         console.error(error);
     });
   }
 
+//   useIntervalAsync(updateState, 3000);
+
   useEffect(() => {
     fetchData();
+    console.log(data)
   }, []);
+
+//   setIntervalAsync(() => {
+//     fetchData();
+//     console.log(1)
+//   }, 3000);
+
 
   return (
     <View style={styles.container}>
@@ -29,13 +70,15 @@ const Listing = () => {
             {data.map(item => (
                 <TouchableOpacity onPress={() => navigation.navigate("Detail", { 
                         route: item.route,
-                        serviceType: item.service_type,
+                        serviceType: item.serviceType,
                         stopId: "BFA3460955AC820C"
                     })}>
                     <View>
                         <Text>{item.route}</Text>
                         <Text>{item.dest_tc}</Text>
-                        <Text>{item.eta}</Text>
+                        {/* <Text>{item.eta}</Text> */}
+                        <Text>{item.minutesLeft !== null && item.minutesLeft >= 0 ? `${Math.floor(item.minutesLeft)} 分鐘` : '暫時沒有班次'}</Text>
+                        <Text> </Text>
                     </View>
                 </TouchableOpacity>
             ))}
